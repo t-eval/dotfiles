e794bc8d32968a203af86f15cb2f023ff04498c9
tommasobruno <bruno.tommaso@protonmail.com>
2024-10-09 11:50:31 +0100

    feat: remove zshrc custom config

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -12,24 +12,5 @@
       # Rest
       ls = "ls -al --color=auto";
     };
-    initExtra = ''
-
-      autoload -Uz vcs_info
-      zstyle ':vcs_info:*' enable git svn
-
-      zstyle ':vcs_info:git*' formats "(%b)"
-      precmd() {
-        vcs_info
-      }
-
-      RPROMP=""
-      setopt PROMPT_SUBST
-
-      if [ -n "$IN_NIX_SHELL" ]; then
-        PROMPT="[%F{red}%n%f@%F{red}%1~%f]%F{yellow}''${vcs_info_msg_0_}%f%F{green}(nix-shell)%f# "
-      else
-        PROMPT="[%F{red}%n%f@%F{red}%1~%f]%F{yellow}''${vcs_info_msg_0_}%f# "
-      fi
-    '';
   };
 }
