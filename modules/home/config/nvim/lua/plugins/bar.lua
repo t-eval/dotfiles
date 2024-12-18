343e309a22ceb76f2230a705d386559b4f5cc7c2
tommasobruno <bruno.tommaso@protonmail.com>
2024-12-18 21:32:49 +0000

    fix: lualine separators

--- modules/home/config/nvim/lua/plugins/bar.lua
+++ modules/home/config/nvim/lua/plugins/bar.lua
@@ -2,9 +2,11 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "|", right = "|" },
-		section_separators = { left = " ", right = " " },
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = " ", right = " " },
+		},
 	},
 }
