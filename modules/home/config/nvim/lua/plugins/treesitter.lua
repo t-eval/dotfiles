70b34f0b509adcfdaeba1c3747fbceadb2bc6e67
tommasobruno <bruno.tommaso@protonmail.com>
2024-12-19 02:16:05 +0000

    feat: add c highlights

--- modules/home/config/nvim/lua/plugins/treesitter.lua
+++ modules/home/config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs",
 	opts = {
-		ensure_installed = { "lua", "nix", "zig", "haskell", "cmake" },
+		ensure_installed = { "lua", "nix", "zig", "haskell", "cmake", "c" },
 
 		auto_install = false,
 		highlight = {
