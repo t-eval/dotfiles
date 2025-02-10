fc76f2babe50410158cb53f6c424321d03050bec
tommasobruno <bruno.tommaso@protonmail.com>
2025-02-10 06:11:36 +0000

    feat: add none-ls for diagnostic

--- modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
+++ modules/home-manager/dotfiles/nvim/lua/plugins/lsp.lua
@@ -22,6 +22,9 @@ return {
       vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, bufopts)
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
       vim.keymap.set("n", "<leader>ct", vim.lsp.buf.type_definition, bufopts)
+
+      -- Diagnostics
+      vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float)
     end
 
     vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
---
+++ modules/home-manager/dotfiles/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        require("none-ls.diagnostics.eslint"),
+      },
+    })
+  end,
+}
